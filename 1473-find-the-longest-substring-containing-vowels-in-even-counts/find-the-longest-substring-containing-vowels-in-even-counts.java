71b4956447c89e17ec90c0b0af08a753132e22df
Gautam Yadav <157649631+Gautam0804@users.noreply.github.com>
2026-08-19 21:19:42 +0530

    Time: 304 ms (6.99%) | Memory: 57.1 MB (5.38%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -1,34 +1,59 @@
 class Solution {
     public int findTheLongestSubstring(String s) {
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        // All vowels have even count before the string starts
-        map.put(0,-1);
-        int mask = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int[] parity = new int[5];
+
+        // All vowels have appeared 0 times
+        map.put("00000", -1);
+
         int maxLength = 0;
 
-        for(int i =0;i<s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
+
             char c = s.charAt(i);
 
-            if(c =='a'){
-                mask ^= (1<<0);
-            } else if(c =='e'){
-                mask ^= (1<<1);
-            }else if(c =='i'){
-                mask ^= (1<<2);
-            }else if(c =='o'){
-                mask ^= (1<<3);
-            }else if(c =='u'){
-                mask ^= (1<<4);
+            // Toggle vowel parity
+            if (c == 'a') {
+                parity[0] = 1 - parity[0];
+            } 
+            else if (c == 'e') {
+                parity[1] = 1 - parity[1];
+            } 
+            else if (c == 'i') {
+                parity[2] = 1 - parity[2];
+            } 
+            else if (c == 'o') {
+                parity[3] = 1 - parity[3];
+            } 
+            else if (c == 'u') {
+                parity[4] = 1 - parity[4];
             }
-            // Look
-            if(map.containsKey(mask)){
-             maxLength = Math.max(maxLength, i-map.get(mask));
-            }else {
-            // Store
-             map.put(mask,i);
+
+            // Create current state
+            String state =
+                    "" + parity[0]
+                    + parity[1]
+                    + parity[2]
+                    + parity[3]
+                    + parity[4];
+
+            // LOOK
+            if (map.containsKey(state)) {
+
+                maxLength = Math.max(
+                        maxLength,
+                        i - map.get(state)
+                );
+
+            } else {
+
+                // STORE earliest index
+                map.put(state, i);
             }
         }
+
         return maxLength;
     }
 }
