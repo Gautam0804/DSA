3e5211198f9abbb95780cc9032d74855d6924d62
Gautam Yadav <157649631+Gautam0804@users.noreply.github.com>
2026-08-20 20:15:46 +0530

    Time: 15 ms (27.49%) | Memory: 47 MB (90.22%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.java
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.java
@@ -4,26 +4,28 @@ class Solution {
         map.put(0,-1);
         int n = hours.length;
         int sum = 0;
-        int count = 0;
+        int longest = 0;
 
         for(int i=0;i<n;i++){
+          
+
             if(hours[i]>8){
                 sum++;
-            }
-            else {
+            } else {
                 sum--;
             }
             if(sum>0){
-                count = i+1;
+                longest = i+1;
             }
             if(map.containsKey(sum-1)){
-                count = Math.max(count,i-map.get(sum-1));
+                longest = Math.max(longest,i-map.get(sum-1));
             }
-            if(!map.containsKey(sum)){
-                map.put(sum,i);
-            }
+           if(!map.containsKey(sum)){
+            map.put(sum,i);
+           }
         }
-        return count;
         
+        
+        return longest;
     }
 }
