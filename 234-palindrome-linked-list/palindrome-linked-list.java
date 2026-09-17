45647a152c920a5a65aaf13555168f876a6ea5b9
Gautam Yadav <157649631+Gautam0804@users.noreply.github.com>
2026-09-17 16:39:00 +0530

    Time: 3 ms (99.80%) | Memory: 94.6 MB (43.05%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,53 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
+    public boolean isPalindrome(ListNode head) {
 
-    public ListNode findMid(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        return slow;
-    }
-
-    public boolean isPalindrome(ListNode head) {
-
-        // Empty list or single node
-        if (head == null || head.next == null) {
-            return true;
-        }
-
-        // Find middle
-        ListNode midNode = findMid(head);
-
-        // Reverse second half
         ListNode prev = null;
-        ListNode curr = midNode;
+        ListNode curr = slow;
 
-        while (curr != null) {
+        while(curr != null){
             ListNode next = curr.next;
-
             curr.next = prev;
             prev = curr;
             curr = next;
+
         }
 
-        ListNode right = prev;
         ListNode left = head;
+        ListNode right = prev;
 
-        // Compare both halves
-        while (right != null) {
-
-            if (left.val != right.val) {
+        while(right != null){
+            if(left.val != right.val){
                 return false;
             }
-
             left = left.next;
             right = right.next;
         }
-
         return true;
+        
     }
 }
